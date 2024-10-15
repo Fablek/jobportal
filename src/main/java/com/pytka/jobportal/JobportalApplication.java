9f8b9b1f5171a22547f5bff7ec42b72ac813a224
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2024-10-15 19:05:27 +0200

    :hammer: Create Controllers, HomeController

--- src/main/java/com/pytka/jobportal/JobportalApplication.java
+++ src/main/java/com/pytka/jobportal/JobportalApplication.java
@@ -1,24 +1,12 @@
 package com.pytka.jobportal;
 
-import org.springframework.ui.Model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-@Controller
 public class JobportalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobportalApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String home(Model model) {
-		model.addAttribute("message", "Witaj na stronie głównej!");
-		return "home";
 	}
 }
