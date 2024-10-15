09e738ed6125535b26ef2a45c8ed311754962f33
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2024-10-15 18:28:09 +0200

    :zap: Maven setup, create home template

--- src/main/java/com/pytka/jobportal/JobportalApplication.java
+++ src/main/java/com/pytka/jobportal/JobportalApplication.java
@@ -1,13 +1,15 @@
 package com.pytka.jobportal;
 
+import org.springframework.ui.Model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class JobportalApplication {
 
 	public static void main(String[] args) {
@@ -15,8 +17,8 @@ public class JobportalApplication {
 	}
 
 	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String home(Model model) {
+		model.addAttribute("message", "Witaj na stronie głównej!");
+		return "home";
 	}
-
 }
